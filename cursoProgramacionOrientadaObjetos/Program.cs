0da2077d2f6546f30406e0b334ddaf589045b8ba
Angel Hernandez <angeldariaux@gmail.com>
2024-08-14 21:34:46 -0600

    creando objetos de superHeroes

--- cursoProgramacionOrientadaObjetos/Program.cs
+++ cursoProgramacionOrientadaObjetos/Program.cs
@@ -1,14 +1,40 @@
 ﻿
-// Asi se declaran los objetos:
-Apuntador apuntadorPlatzi = new Apuntador();
-Apuntador apuntadorPlatzi2 = new Apuntador();
+// Creando nuevo objeto para superman
+var superman = new SuperHeroe();
 
-apuntadorPlatzi.Color = "Negro";
-apuntadorPlatzi2.Color = "Blanco";
+superman.Id = 1;
+superman.Nombre = "Superman";
+superman.IdentidadSecreta = "Clark Kent";
+superman.Ciudad = "Metropolis";
+superman.PuedeVolar = true;
+superman.SuperPoderes = new [] {"vision de rayos x", "Volar", "Super Fuerza", "Ojos rayos lazer"};
 
-class Apuntador { 
-  public string Color;
-  public double Largo;
-  public short NumeroDeBotones;
-  public bool TieneStickers;
+// Creando nuevo objeto para Batman
+var batman = new SuperHeroe();
+
+batman.Id = 2;
+batman.Nombre = "Batman";
+batman.IdentidadSecreta = "Bruce Wayne";
+batman.Ciudad = "Gotham";
+batman.PuedeVolar= false;
+batman.SuperPoderes = new [] {"inteligencia superior", "Habilidades de detective", "Artes marciales", "Tenologia avanzada"};
+
+// Creando nuevo objeto para Wonder woman
+var wonderWoman = new SuperHeroe();
+
+wonderWoman.Id = 3;
+wonderWoman.Nombre= "Wonder Woman";
+wonderWoman.IdentidadSecreta= "Diana Prince";
+wonderWoman.Ciudad= "Themyscira";
+wonderWoman.PuedeVolar = true;
+wonderWoman.SuperPoderes= new [] {"Super Fuerza", "Inmortalidad", "Habilidad con el lazo de la verdad", "Velocidad"};
+
+
+class SuperHeroe { 
+  public int Id;
+  public string Nombre;
+  public string IdentidadSecreta;
+  public string Ciudad;
+  public string [] SuperPoderes;
+  public bool PuedeVolar;
 }
