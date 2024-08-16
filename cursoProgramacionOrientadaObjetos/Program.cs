7a0c45823712b58d67866756563a26eeee4b5eb2
Angel Hernandez <angeldariaux@gmail.com>
2024-08-15 21:18:55 -0600

    usando enum, y clases para mejorar los objetos

--- cursoProgramacionOrientadaObjetos/Program.cs
+++ cursoProgramacionOrientadaObjetos/Program.cs
@@ -1,4 +1,37 @@
 ﻿
+using System.Numerics;
+
+var volar = new SuperPoder();
+volar.Nombre = "Volar";
+volar.Descripcion = "Capacidad para volar y planear en el aire";
+volar.Nivel = NivelPoder.NivelDos;
+
+var superFuerza = new SuperPoder();
+superFuerza.Nombre = "super fuerza";
+superFuerza.Descripcion = "Fuerza sobrehumana";
+superFuerza.Nivel = NivelPoder.NivelTres;
+
+var superInteligencia = new SuperPoder();
+superInteligencia.Nombre = "Super Inteligencia";
+superInteligencia.Descripcion= "Inteligencia muy superior a la de los humanos";
+superInteligencia.Nivel= NivelPoder.NivelUno;
+
+var tecnologia = new SuperPoder();
+tecnologia.Nombre= "Tecnologia muy avanzada";
+tecnologia.Descripcion= "Tecnologia muy avanzada desarrollada por humanos o Aliens";
+tecnologia.Nivel= NivelPoder.NivelDos;
+
+var velocidad = new SuperPoder();
+velocidad.Nombre = "Velocidad";
+velocidad.Descripcion= "Velicidad sobrehumana";
+velocidad.Nivel = NivelPoder.NivelTres;
+
+var inmotalidad = new SuperPoder();
+inmotalidad.Nombre= "Inmortalidad";
+inmotalidad.Descripcion = "Capazcidad de no morir";
+inmotalidad.Nivel = NivelPoder.NivelTres;
+
+
 // Creando nuevo objeto para superman
 var superman = new SuperHeroe();
 
@@ -7,7 +40,12 @@ superman.Nombre = "Superman";
 superman.IdentidadSecreta = "Clark Kent";
 superman.Ciudad = "Metropolis";
 superman.PuedeVolar = true;
-superman.SuperPoderes = new [] {"vision de rayos x", "Volar", "Super Fuerza", "Ojos rayos lazer"};
+
+List<SuperPoder> poderesSuperman = new List<SuperPoder>();  // Aqui se declara que los super poders seran una lista de la clase "superPoder".
+poderesSuperman.Add(volar);  // aqui se le agregan a lista de los superpoderes de superman el poder de "poderVolar"
+poderesSuperman.Add(superFuerza);
+
+superman.SuperPoderes = poderesSuperman;  // Se agrega todo el objeto de superpoderes a Superman. 
 
 // Creando nuevo objeto para Batman
 var batman = new SuperHeroe();
@@ -17,7 +55,12 @@ batman.Nombre = "Batman";
 batman.IdentidadSecreta = "Bruce Wayne";
 batman.Ciudad = "Gotham";
 batman.PuedeVolar= false;
-batman.SuperPoderes = new [] {"inteligencia superior", "Habilidades de detective", "Artes marciales", "Tenologia avanzada"};
+
+List<SuperPoder> poderesBatman = new List<SuperPoder>();
+poderesBatman.Add(superInteligencia);
+poderesBatman.Add(tecnologia);
+
+batman.SuperPoderes = poderesBatman;
 
 // Creando nuevo objeto para Wonder woman
 var wonderWoman = new SuperHeroe();
@@ -27,7 +70,12 @@ wonderWoman.Nombre= "Wonder Woman";
 wonderWoman.IdentidadSecreta= "Diana Prince";
 wonderWoman.Ciudad= "Themyscira";
 wonderWoman.PuedeVolar = true;
-wonderWoman.SuperPoderes= new [] {"Super Fuerza", "Inmortalidad", "Habilidad con el lazo de la verdad", "Velocidad"};
+
+List<SuperPoder> poderesWonderWoman = new List<SuperPoder>();
+poderesWonderWoman.Add(inmotalidad);
+poderesWonderWoman.Add(superFuerza);
+
+wonderWoman.SuperPoderes = poderesWonderWoman;
 
 
 class SuperHeroe { 
@@ -35,6 +83,18 @@ class SuperHeroe {
   public string Nombre;
   public string IdentidadSecreta;
   public string Ciudad;
-  public string [] SuperPoderes;
+  public List<SuperPoder> SuperPoderes;  // List<T> es una clase genérica en C# que representa una lista (o colección) de objetos de un tipo específico. En este caso, SuperPoder es el tipo de objeto que la lista va a contener. 
   public bool PuedeVolar;
+}
+
+class SuperPoder { 
+  public string Nombre;
+  public string Descripcion;
+  public NivelPoder Nivel;  // Cada que queramos asignarle un nivel de poder deberemos usar la clase "NivelPoder".
+}
+
+enum NivelPoder { 
+  NivelUno, 
+  NivelDos, 
+  NivelTres
 }
