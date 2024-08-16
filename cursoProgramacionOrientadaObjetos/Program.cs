bc747c6acb1d1055a9cb2f0ed6b3bf97182b58f2
Angel Hernandez <angeldariaux@gmail.com>
2024-08-15 21:33:19 -0600

    creando constructores

--- cursoProgramacionOrientadaObjetos/Program.cs
+++ cursoProgramacionOrientadaObjetos/Program.cs
@@ -85,12 +85,23 @@ class SuperHeroe {
   public string Ciudad;
   public List<SuperPoder> SuperPoderes;  // List<T> es una clase genérica en C# que representa una lista (o colección) de objetos de un tipo específico. En este caso, SuperPoder es el tipo de objeto que la lista va a contener. 
   public bool PuedeVolar;
+
+  public SuperHeroe() {
+    Id = 1;  // el ID por defecto sera 1
+    SuperPoderes = new List<SuperPoder>();
+    PuedeVolar = false;
+  }
 }
 
 class SuperPoder { 
   public string Nombre;
   public string Descripcion;
   public NivelPoder Nivel;  // Cada que queramos asignarle un nivel de poder deberemos usar la clase "NivelPoder".
+
+  public SuperPoder() {
+    Nivel = NivelPoder.NivelUno;  // el nivel de poder por defecto sera 1.
+
+  }
 }
 
 enum NivelPoder { 
