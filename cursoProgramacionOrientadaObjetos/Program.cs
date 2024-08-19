3ba4a247bd6245d8b5bfcc2b6223da1a114a0723
Angel Hernandez <angeldariaux@gmail.com>
2024-08-18 20:46:20 -0600

    tipos de datos, condiciones logicas

--- cursoProgramacionOrientadaObjetos/Program.cs
+++ cursoProgramacionOrientadaObjetos/Program.cs
@@ -1,111 +1,19 @@
-﻿
-using System.Numerics;
-
-var volar = new SuperPoder();
-volar.Nombre = "Volar";
-volar.Descripcion = "Capacidad para volar y planear en el aire";
-volar.Nivel = NivelPoder.NivelDos;
-
-var superFuerza = new SuperPoder();
-superFuerza.Nombre = "super fuerza";
-superFuerza.Descripcion = "Fuerza sobrehumana";
-superFuerza.Nivel = NivelPoder.NivelTres;
-
-var superInteligencia = new SuperPoder();
-superInteligencia.Nombre = "Super Inteligencia";
-superInteligencia.Descripcion= "Inteligencia muy superior a la de los humanos";
-superInteligencia.Nivel= NivelPoder.NivelUno;
-
-var tecnologia = new SuperPoder();
-tecnologia.Nombre= "Tecnologia muy avanzada";
-tecnologia.Descripcion= "Tecnologia muy avanzada desarrollada por humanos o Aliens";
-tecnologia.Nivel= NivelPoder.NivelDos;
-
-var velocidad = new SuperPoder();
-velocidad.Nombre = "Velocidad";
-velocidad.Descripcion= "Velicidad sobrehumana";
-velocidad.Nivel = NivelPoder.NivelTres;
-
-var inmotalidad = new SuperPoder();
-inmotalidad.Nombre= "Inmortalidad";
-inmotalidad.Descripcion = "Capazcidad de no morir";
-inmotalidad.Nivel = NivelPoder.NivelTres;
+﻿int a = 20100000;
+int b = 30000000;
+long c = (long)a + (long)b;  // (long) es una conversion explicita o "cast" para pasar el dato de tipo "int" a tipo "long".
+Console.WriteLine(c);
 
 
-// Creando nuevo objeto para superman
-var superman = new SuperHeroe();
 
-superman.Id = 1;
-superman.Nombre = "Superman";
-superman.IdentidadSecreta = "Clark Kent";
-superman.Ciudad = "Metropolis";
-superman.PuedeVolar = true;
+double aa = 42.1; // Natural type
+float bb = 38.2F;  // La "F" es para convertir el tipo natural de "double" al tipo "float"
+double cc = aa + bb;
+Console.WriteLine(cc);
 
-List<SuperPoder> poderesSuperman = new List<SuperPoder>();  // Aqui se declara que los super poders seran una lista de la clase "superPoder".
-poderesSuperman.Add(volar);  // aqui se le agregan a lista de los superpoderes de superman el poder de "poderVolar"
-poderesSuperman.Add(superFuerza);
+decimal a1 = 42.1M;  // Explicit type
+decimal a2 = 38.2M;
+decimal b1 = a1 + a2;
+Console.WriteLine(b1);
 
-superman.SuperPoderes = poderesSuperman;  // Se agrega todo el objeto de superpoderes a Superman. 
+// Conditional logic 
 
-// Creando nuevo objeto para Batman
-var batman = new SuperHeroe();
-
-batman.Id = 2;
-batman.Nombre = "Batman";
-batman.IdentidadSecreta = "Bruce Wayne";
-batman.Ciudad = "Gotham";
-batman.PuedeVolar= false;
-
-List<SuperPoder> poderesBatman = new List<SuperPoder>();
-poderesBatman.Add(superInteligencia);
-poderesBatman.Add(tecnologia);
-
-batman.SuperPoderes = poderesBatman;
-
-// Creando nuevo objeto para Wonder woman
-var wonderWoman = new SuperHeroe();
-
-wonderWoman.Id = 3;
-wonderWoman.Nombre= "Wonder Woman";
-wonderWoman.IdentidadSecreta= "Diana Prince";
-wonderWoman.Ciudad= "Themyscira";
-wonderWoman.PuedeVolar = true;
-
-List<SuperPoder> poderesWonderWoman = new List<SuperPoder>();
-poderesWonderWoman.Add(inmotalidad);
-poderesWonderWoman.Add(superFuerza);
-
-wonderWoman.SuperPoderes = poderesWonderWoman;
-
-
-class SuperHeroe { 
-  public int Id;
-  public string Nombre;
-  public string IdentidadSecreta;
-  public string Ciudad;
-  public List<SuperPoder> SuperPoderes;  // List<T> es una clase genérica en C# que representa una lista (o colección) de objetos de un tipo específico. En este caso, SuperPoder es el tipo de objeto que la lista va a contener. 
-  public bool PuedeVolar;
-
-  public SuperHeroe() {
-    Id = 1;  // el ID por defecto sera 1
-    SuperPoderes = new List<SuperPoder>();
-    PuedeVolar = false;
-  }
-}
-
-class SuperPoder { 
-  public string Nombre;
-  public string Descripcion;
-  public NivelPoder Nivel;  // Cada que queramos asignarle un nivel de poder deberemos usar la clase "NivelPoder".
-
-  public SuperPoder() {
-    Nivel = NivelPoder.NivelUno;  // el nivel de poder por defecto sera 1.
-
-  }
-}
-
-enum NivelPoder { 
-  NivelUno, 
-  NivelDos, 
-  NivelTres
-}
