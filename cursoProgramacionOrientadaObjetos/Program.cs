7e9a1a9d73b3551e2b6894af6f3a2f7fdc95c0a0
Angel Hernandez <angeldariaux@gmail.com>
2024-08-18 21:04:41 -0600

    operaionces logias con AND

--- cursoProgramacionOrientadaObjetos/Program.cs
+++ cursoProgramacionOrientadaObjetos/Program.cs
@@ -17,3 +17,27 @@ Console.WriteLine(b1);
 
 // Conditional logic 
 
+int value1 = 5;
+int value2 = 4;
+int value3 = value1 + value2;
+int value4 = 10;
+
+
+bool myTest = value3 > 10;  // will be true or false
+
+if ((value1 + value2) > 10) {
+  Console.WriteLine("The answer is greater than 10.");
+}
+
+
+if (myTest) { // is true
+  Console.WriteLine("The answer is greater than 10, but this time using the bool type.");
+}
+else { 
+  Console.WriteLine("The answer is LESS than 10.");
+}
+
+if ((value1 + value2 + value3 > 10) && (value4 == 10)) {
+  Console.WriteLine("The Logic of 'AND' is working!");
+}
+
